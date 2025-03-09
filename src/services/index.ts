f552d46ad110c05649351e275cb7131c27ec7e49
poom-yutthapichai <o_yutthapichai.c@kubix.co>
2025-03-10 06:46:51 +0700

    feat:add redis cache

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,16 +1,36 @@
 import { Products } from "../types";
 import axios from "axios";
+import ioredis from "ioredis";
 export class ProductsService {
-  constructor() {}
+  private readonly redis: ioredis;
+  constructor() {
+    this.redis = new ioredis();
+  }
 
   async getProducts(): Promise<Products> {
-    const protducts = await axios
-      .get("https://fgt9jf-8080.csb.app/getProducts")
-      .then((response) => {
+    const url = "https://fgt9jf-8080.csb.app/getProducts";
+
+    const cacheKey = `cache:${url}`;
+    const cacheTTL = 60 * 1;
+
+    const cachedData = await this.redis.get(cacheKey);
+
+    if (cachedData) {
+      console.log("📌 Data from cache");
+      return JSON.parse(cachedData);
+    }
+
+    try {
+      const protducts = await axios.get(url).then((response) => {
         console.log({ response: response.data });
         return response.data;
       });
 
-    return protducts;
+      await this.redis.set(cacheKey, JSON.stringify(protducts), "EX", cacheTTL);
+
+      return protducts;
+    } catch (error) {
+      throw error;
+    }
   }
 }
