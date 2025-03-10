da99ccbdb76f0e5534be54f4b05eaf820734c544
poom-yutthapichai <o_yutthapichai.c@kubix.co>
2025-03-10 13:22:33 +0700

    feat:add helmet

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import helmet from "helmet";
 import { routers } from "./routes/api";
 
 const port = process.env.PORT || 8000;
@@ -8,6 +9,7 @@ const api_version = process.env.API_VERSION || "v1";
 const app = express();
 app.use(express.json());
 
+app.use(helmet());
 app.use(cors());
 
 app.use("/"+api_version, routers);
